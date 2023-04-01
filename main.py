ba791715445702997aed7861c7c0c7bbb54a35c5
Ian Miller <imiller142@gmail.com>
2023-04-01 13:40:35 -0500

    Refined code and completed game.

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import time
 # for card generation
 suits = ['hearts', 'diamonds', 'spades', 'clubs']
 values = ['ace', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'jack', 'queen', 'king']
-current_bet = 0
+
 #define player class
 class Player:
 
@@ -12,6 +12,8 @@ class Player:
         self.name = name
         self.money = int(money)
         self.hand =  []
+        self.play_state = True
+        self.current_bet = 0
 
 class Dealer:
 
@@ -49,24 +51,26 @@ def create_deck():
 #player = Player(input("What is your name? "), input("How much money will you be playing with? "))
 player = Player('ian', 100)
 def play_round():
+    player.current_bet = 0
 
+    current_deck = []
     current_deck = create_deck()
-
+    time.sleep(2)
+    player.hand = []
+    dealer.hand = []
     #function to get the current bet and check if its valid
     def get_current_bet():
-        current_bet = 0
         valid_bet = False
         while not valid_bet:
-            current_bet = int(input('How much do you want to bet this round? '))
+            current_bet = int(input(f' You currently have {player.money} dollars. How much do you want to bet this round? '))
+            time.sleep(1)
             if player.money - current_bet >= 0:
-                player.money = player.money - current_bet
                 valid_bet = True
             else:
                 print('Sorry that is an invalid amount.')
         return current_bet
     
-    current_bet = get_current_bet()
-
+    player.current_bet = get_current_bet()
 
     #will deal the cards to the instanced hands.
     def deal():
@@ -90,61 +94,84 @@ def play_round():
 
     #Does player want to hit or stay or double down
 
-    #compare hands
-    def hand_compare(phand, dhand):
-        if sum_hand(phand) >= sum_hand(dhand) and sum_hand(phand) <= 21:
-            player.money += current_bet * 2
-            return 'You have won this round'
-        elif sum_hand(phand) < sum_hand(dhand):
-            player.money -= current_bet
-            return 'You have lost'
-        else:
-            return 'Bust! you have lost this round'
-    
+
     def hit(hand):
         hand.hand.append(current_deck.pop(random.randint(0, len(current_deck))))
-        print(f'{hand.name} was dealt a {hand.hand[2]} your hand total is now {sum_hand(hand.hand)}')
+        print(f'{hand.name} was dealt a {hand.hand[-1]} for a hand total of {sum_hand(hand.hand)}')
 
     #this is the player move set
     def move_set():
         turn_over = False
         decision = input("Do you want to hit stand or double down? ")
-        while not turn_over:
-            #loop to check players move. Lots of comparatives to see if hand ever enters bust state.
-            if decision.lower() == 'hit':
-                hit(player)
-                if sum_hand(player.hand) > 21:
-                    break
-                else:
-                    question = input('Would you like to hit or stand? ')
-                    while question.lower() != 'stand':
-                        hit(player)
-                        question = input('Would you like to hit or stand? ')
-                        if sum_hand(player.hand) > 21:
-                            question = 'stand'
-            elif decision.lower() == 'double down':
-                hit(player)
-                current_bet = current_bet * 2
-            else:
-                turn_over = True
 
-            turn_over = True
+        time.sleep(1)
+            #loop to check players move. Lots of comparatives to see if hand ever enters bust state.
+        if decision.lower() == 'hit':
+            hit(player)
+            if sum_hand(player.hand) > 21:
+                turn_over = True
+            elif sum_hand(player.hand) < 21:
+                while not turn_over:
+                    question = input('Would you like to hit or stand? ')
+                    time.sleep(1)
+                    if question.lower() == 'hit': 
+                        hit(player)
+                        if sum_hand(player.hand) > 21:
+                            print(f'You were dealt a {player.hand[-1]}')
+                            turn_over = True
+                else:
+                    turn_over = True
+        elif decision.lower() == 'double down':
+            hit(player)
+            player.current_bet = player.current_bet * 2
+        
+        else:
+            return
 
     def dealer_move():
         print(f'The dealer reveals their cards showing a {dealer.hand[0]} and a {dealer.hand[1]} for a total of {sum_hand(dealer.hand)}')
         time.sleep(1)
-        while sum_hand(dealer.hand) <= 16:
+        while sum_hand(dealer.hand) <= 16 and sum_hand(player.hand) < 21:
+            print(f'The dealer has 16 or less so the they hit.')
             hit(dealer)
-            print(f'The dealer has less than 16 so they hit and receive a {dealer.hand[-1]} for a new hand total of {sum_hand(dealer.hand)}')
             time.sleep(1)
+
+        #compare hands
+    def hand_compare(phand, dhand):
+        if sum_hand(phand) >= sum_hand(dhand) and sum_hand(phand) <= 21:
+            player.money += player.current_bet * 2
+            return 'You have won this round'
+        elif sum_hand(phand) < sum_hand(dhand) and sum_hand(dhand) <= 21:
+            player.money -= player.current_bet
+            return 'You have lost'
+        elif sum_hand(dhand) > 21 :
+            player.money += player.current_bet
+            return 'The dealer has busted. You win this round.'
+        else:
+            player.money -= player.current_bet
+            return 'Bust! you have lost this round'
+    
     
     deal()
     print(f'You currently have the {player.hand[0]} and the {player.hand[1]} for a total of {sum_hand(player.hand)}')
     print(f'The dealer is currently showing the {dealer.hand[1]}')
 
+ 
     move_set()
     dealer_move()
     time.sleep(2)
   
     print(hand_compare(player.hand, dealer.hand) + f' you had a total of {sum_hand(player.hand)} while the dealer had {sum_hand(dealer.hand)} you now have {player.money} dollars left.')
-play_round()
+
+def game():
+    10
+    while player.play_state == True:
+        play_round()
+        if player.money <= 0:
+            print('You are out of money. Come back with more money to play again.')
+            player.play_state = False
+        play_again = input('Would you like to play another round? ')
+        if play_again.lower() == 'no':
+            player.play_state = False
+
+game()
